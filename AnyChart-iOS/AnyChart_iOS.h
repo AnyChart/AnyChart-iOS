75c03ec927c45b921fe7fb71dc4637bec9467fd4
NoName <noname@nonames-imac.local>
2018-10-16 17:18:01 +0800

    Initial Commit

---
+++ AnyChart-iOS/AnyChart_iOS.h
@@ -0,0 +1,19 @@
+//
+//  AnyChart_iOS.h
+//  AnyChart-iOS
+//
+//  Created by NoName on 10/16/18.
+//  Copyright © 2018 AnyChart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnyChart_iOS.
+FOUNDATION_EXPORT double AnyChart_iOSVersionNumber;
+
+//! Project version string for AnyChart_iOS.
+FOUNDATION_EXPORT const unsigned char AnyChart_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnyChart_iOS/PublicHeader.h>
+
+
